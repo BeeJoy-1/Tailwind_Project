9e4d660f50d391bc83d91da6c8e4c643b81adfa1
BeeJoy-1 <beejoy3000@gmail.com>
2023-12-25 11:41:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#FFD2A4',
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+        fontFamily: {
+            'Roboto': "'Roboto Condensed', sans-serif",
+        },
+        backgroundImage: {
+            'banner': "url('images/women.jpg')",
+        }
+    }
+  }
+}
