80c57b9d7b5ff86f54f424a95c5b5bc5e433e26f
BeeJoy-1 <beejoy3000@gmail.com>
2023-12-26 11:14:49 +0600

    did another section

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ tailwind.config = {
         },
         backgroundImage: {
             'banner': "url('images/women.jpg')",
+            'church': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/pray.png')",
+            'book': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/book.png')",
+            'read': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/read.png')",
+            'glass': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/glass.png')",
         }
     }
   }
