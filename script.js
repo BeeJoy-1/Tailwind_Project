ff12237b9f6baada80b4a98a6df5f03057fb9a5a
BeeJoy-1 <beejoy3000@gmail.com>
2023-12-29 20:38:49 +0600

    added another section after a while

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ tailwind.config = {
             'book': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/book.png')",
             'read': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/read.png')",
             'glass': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%) ,url('images/glass.png')",
+            'angels': "url('images/angels.jpg')",
+            'temple': "url('images/temple.jpg'),linear-gradient(110deg, #CD5423 -53.83%, #FFD0A0 62.61%)",
         }
     }
   }
